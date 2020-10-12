ad52af44bf8fa7656d4da3cd6428d78dd64cfcc9
Harrison King <authorharrisonking@gmail.com>
2020-10-12 17:42:52 -0600

    looper

--- public/src/Footer/footer.js
+++ public/src/Footer/footer.js
@@ -5,6 +5,13 @@ import { AiOutlineTwitter } from 'react-icons/ai';
 import { AiOutlineInstagram } from 'react-icons/ai';
 import { AiOutlineYoutube } from 'react-icons/ai';
 
+let styles = {
+    'font-size': 30,
+    'margin-left': 5,
+    'margin-right': 5,
+    'margin-bottom': 5,
+};
+
 let footerVars = {
     nav: [
         {
@@ -37,39 +44,43 @@ let footerVars = {
         },
     ],
     socialMedia: [
-        '#',
-        '#',
-        '#',
-        '#',
+        {
+            name: (<AiOutlineFacebook style={styles}/>),
+            link: '#',
+        },        
+        {
+            name: (<AiOutlineTwitter style={styles}/>),
+            link: '#',
+        },        
+        {
+            name: (<AiOutlineInstagram style={styles}/>),
+            link: '#',
+        },        
+        {
+            name: (<AiOutlineYoutube style={styles}/>),
+            link: '#',
+        },
     ],
 };
 
 const Footer = () => {   
-    let styles = {
-        'font-size': 30,
-        'margin-left': 5,
-        'margin-right': 5,
-        'margin-bottom': 5,
-    };
+    const itemMap = (arr, target, icon) => {
+        if (target) {
+            return arr.map(i => <a href={i.link} target='_blank'>{i.name}</a>)
+        }
+        return arr.map(i => <a href={i.link}> • {i.name}</a>)
+    }
+
     return (
         <div id='footer'>
             <p id='footer-nav'>
-                <a href={footerVars.nav[0].link}>{footerVars.nav[0].name}</a> • 
-                <a href={footerVars.nav[1].link}>{footerVars.nav[1].name}</a> • 
-                <a href={footerVars.nav[2].link}>{footerVars.nav[2].name}</a> • 
-                <a href={footerVars.nav[3].link}>{footerVars.nav[3].name}</a> • 
-                <a href={footerVars.nav[4].link}>{footerVars.nav[4].name}</a>  • 
-                <a href={footerVars.nav[5].link}>{footerVars.nav[5].name}</a> • 
-                <a href={footerVars.nav[6].link}>{footerVars.nav[6].name}</a>
+                {itemMap(footerVars.nav)}
             </p>
 
             <div id='footer-line-break'/>
 
             <p id='social-media'>
-                <a href={footerVars.socialMedia[0]} target="_blank"><AiOutlineFacebook style={styles}/></a>
-                <a href={footerVars.socialMedia[1]} target="_blank"><AiOutlineTwitter style={styles}/></a>
-                <a href={footerVars.socialMedia[2]} target="_blank"><AiOutlineInstagram style={styles}/></a>
-                <a href={footerVars.socialMedia[3]} target="_blank"><AiOutlineYoutube style={styles}/></a>
+                {itemMap(footerVars.socialMedia, true, true)}
             </p>
         </div>
     )
